c3c81743a1ea489fc2c77cb14e39c3d53be278bc
Konstantin <konstant.kovalenko@gmail.com>
2022-01-07 22:41:12 +0300

    Add category repository

---
+++ Kokos_DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Kokos_DataAccess.Data;
+using Kokos_DataAccess.Repository.IRepository;
+using Kokos_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kokos_DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category obj)
+        {
+            var objFromDb = base.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.DisplayOrder = obj.DisplayOrder;
+            }
+        }
+    }
+}
